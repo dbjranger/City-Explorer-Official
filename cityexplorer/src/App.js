b2cf9b640846e8dec4115d60eeb0efbc5b584c16
don <bandy@gmail.com>
2021-07-28 20:13:32 -0700

    completed lat, lon, display name UserStory2

--- cityexplorer/src/App.js
+++ cityexplorer/src/App.js
@@ -3,34 +3,44 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-  // constructor(props){
-  //   this.state = {
-  //     city: ' ',
-  //     renderLatLon: false,
-  //     lat: 0,
-  //     lon: 0,
-  //   }
-  // }
+  constructor(props){
+    super(props);
+    this.state = {
+      searchQuery: ' ',
+      renderLatLon: false,
+      lat: 0,
+      lon: 0,
+      display_name: '',
+      locationResult: {}
+    }
+  }
 
-  // handleChange = (e) => {
-  //   this.setState({city: e.target.value})
-  // }
+  onChange = (e) => {
+    this.setState({searchQuery: e.target.value})
+  }
 
-  // getCity = (e) => {
-  //   e.preventDefault();
-
-  //   let cityResults = await axios.get('https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.city.state}&format=json');
-  //   return cityResults;
-  // }
+  getCity = async (e) => {
+    e.preventDefault();
+    let cityResults = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`);
+    console.log(cityResults);
+    this.setState({locationResult: cityResults.data[0]});
+    this.setState({lat: cityResults.data[0].lat});
+    this.setState({lon: cityResults.data[0].lon});
+    this.setState({display_name: cityResults.data[0].display_name})
+  }
 
   render() {
+    console.log(this.state)
     return (
       <>
-        <h1>Hello World</h1>
+        <h1>City Explorer</h1>
         <form onSubmit={this.getCityInfo}>
-          <input onChange={this.handleChange}/>
-          <button>Get City Information</button>
+          <input onChange={ (e) => this.setState({searchQuery: e.target.value}) }></input>      
+          <button onClick={this.getCity}>Explore!</button>
         </form>
+        <h2>{this.state.display_name}</h2>
+        <h3>The latitude is: {this.state.lat}</h3>
+        <h3>The longitude is: {this.state.lon}</h3>
       </>
     )
   }
