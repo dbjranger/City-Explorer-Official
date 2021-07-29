660b41114de2fe1e8b3ce46ed72d2bec9ba02de2
don <bandy@gmail.com>
2021-07-28 21:17:38 -0700

    showing map

--- cityexplorer/src/App.js
+++ cityexplorer/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 class App extends React.Component {
   constructor(props){
@@ -10,6 +12,7 @@ class App extends React.Component {
       renderLatLon: false,
       lat: 0,
       lon: 0,
+      showMap: '',
       display_name: '',
       locationResult: {}
     }
@@ -21,12 +24,18 @@ class App extends React.Component {
 
   getCity = async (e) => {
     e.preventDefault();
-    let cityResults = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`);
-    console.log(cityResults);
+    
+    let cityResults = await axios.get(`https://us1.locationiq.com/v1/search.php?
+      key=${process.env.REACT_APP_LOCATIONIQ_KEY}&
+      q=${this.state.searchQuery}&
+      format=json`);
+      console.log(cityResults);
+
     this.setState({locationResult: cityResults.data[0]});
     this.setState({lat: cityResults.data[0].lat});
     this.setState({lon: cityResults.data[0].lon});
-    this.setState({display_name: cityResults.data[0].display_name})
+    this.setState({display_name: cityResults.data[0].display_name});
+    this.setState({showMap: cityResults.data[0].showMap});
   }
 
   render() {
@@ -34,13 +43,20 @@ class App extends React.Component {
     return (
       <>
         <h1>City Explorer</h1>
-        <form onSubmit={this.getCityInfo}>
-          <input onChange={ (e) => this.setState({searchQuery: e.target.value}) }></input>      
-          <button onClick={this.getCity}>Explore!</button>
-        </form>
-        <h2>{this.state.display_name}</h2>
+        {/* Bootstrap Form and Button not working */}
+        <div>
+          <Form onSubmit={this.getCityInfo}> 
+              <input onChange={ (e) => this.setState({searchQuery: e.target.value}) }></input>      
+              <Button onClick={this.getCity} variant="primary">Explore!</Button>
+          </Form>
+        </div>
+          <h2>{this.state.display_name}</h2>
         <h3>The latitude is: {this.state.lat}</h3>
         <h3>The longitude is: {this.state.lon}</h3>
+        <img src={`https://maps.locationiq.com/v3/staticmap?
+          key=${process.env.REACT_APP_LOCATIONIQ_KEY}&
+          center=${this.state.lat},${this.state.lon}
+          &zoom=10`} alt={this.state.display_name}/>
       </>
     )
   }
